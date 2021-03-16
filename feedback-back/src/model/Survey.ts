b223b577103a1fe9fbfa501beb9b504d2ec1ac9e
Masum Islam <madsum@gmail.com>
2021-03-16 12:59:34 +0200

    Implement Survey model

---
+++ feedback-back/src/model/Survey.ts
@@ -0,0 +1,8 @@
+export class Survey{
+    public question1: String;
+    public answer1: String;
+    public question2 : String;
+    public answer2: String;
+    public question3 : String;
+    public answer3: String;
+}
