3f8d4c062525d3bcc61134a69b721d5fafaa1295
Masum Islam <madsum@gmail.com>
2021-03-16 15:12:53 +0200

    Implement SurveyForm1 page

---
+++ feedback-front/src/component/Constant.js
@@ -0,0 +1,6 @@
+export const question1 =
+  "How likely are you going to recommend our service to a friend or colleague?";
+export const question3 = "How likely will you continue using our service?";
+export const surveyTitle = "Trustmary Dev";
+export const answerThanks = "Thank you for answering!";
+export const feedbackThanks = "Your feedback is appreciated.";
---
+++ feedback-front/src/component/SurveyForm1/SurveyForm1.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import axios from "axios";
+import history from "../../history";
+import { surveyTitle } from "../Constant";
+import "../SurveyFormCommon.css";
+
+export class SurveyForm1 extends Component {
+  question = "";
+  ratings = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedRatingIndex: -1,
+      question1: this.props.question1,
+      question3: this.props.question3,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  renderRating = () =>
+    this.ratings.map((val) => (
+      <button
+        key={val}
+        className={
+          this.state.selectedRatingIndex === val
+            ? "selectedButton"
+            : "rate-range-button"
+        }
+        onClick={(e) => {
+          e.preventDefault();
+
+          this.setState({
+            selectedRatingIndex: val,
+          });
+        }}
+      >
+        {val}
+      </button>
+    ));
+
+  handleSubmit(event) {
+    event.target.reset();
+    event.preventDefault();
+
+    let data = "";
+    let nextPage = "";
+
+    if (this.state.selectedRatingIndex === -1) {
+      alert("Please select at least one rate!");
+      return;
+    }
+
+    if (this.state.question1) {
+      data = {
+        questionOneAnswer: this.state.selectedRatingIndex,
+      };
+      nextPage = "/question2";
+    } else if (this.state.question3) {
+      data = {
+        questionThreeAnswer: this.state.selectedRatingIndex,
+      };
+      nextPage = "/thanks";
+    }
+
+    axios
+      .post("http://localhost:4000/addFeedback", data, {})
+      .then((res) => {
+        history.push({
+          pathname: nextPage,
+          state: {
+            response: res.data,
+          },
+        });
+      })
+      .catch((error) => {
+        console.log("error: " + error);
+      });
+  }
+
+  render() {
+    if (this.state.question1) {
+      this.question = this.state.question1;
+    } else if (this.state.question3) {
+      this.question = this.state.question3;
+    }
+    return (
+      <div>
+        <div className="survey-form-header-name">{surveyTitle}</div>
+        <hr></hr>
+        <form onSubmit={this.handleSubmit}>
+          <label className="survey-question-label" title="Field is required">
+            <div>
+              {this.question}
+              <span className="field-label-required">*</span>
+            </div>
+            <div></div>
+          </label>
+          <div>{this.renderRating()}</div>
+          <div className="range-labels-text">
+            <div className="range-labels-text-max">
+              Very likely&nbsp;&nbsp;&nbsp;
+            </div>
+            <div className="range-labels-text-min">Very unlikely</div>
+          </div>
+          <div className="survey-form-submit">
+            <button className="survey-form-submit-button" type="submit">
+              Next
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SurveyForm1;
